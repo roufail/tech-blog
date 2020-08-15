6e9bdf694ce4d1ec95fc2f32566be8a4e49d1964
osama <osama.saieed@gmail.com>
2020-08-15 20:01:41 +0200

    put frontend files

---
+++ resources/views/frontend/homepage.blade.php
@@ -0,0 +1,1 @@
+@extends('frontend.template.master')
--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
-// Route::get('/admin', function () {
-//     return view('admin.dashboard');
-// });
-
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['namespace' => 'Frontend','prefix' =>'','as'=>'frontend.'],function(){
+    Route::get("/","FrontendController@home_page");
+});
+
 
 
 
